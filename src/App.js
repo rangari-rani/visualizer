5ec14f5cc2c2f29354e4cb842084a4f5ddc12062
RANI RANGARI <ranirangari2@gmail.com>
2025-07-17 16:19:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import SortingVisualizer from './components/SortingVisualizer';
 function App() {
   return (
     <div className="App">
-      <SortingVisualizer></SortingVisualizer>
+      <SortingVisualizer />
+
+      <footer className="footer">
+        <p>Built by Rani Rangari • MCA Project • 2022</p>
+        <p>
+          <a href="https://github.com/rangari-rani/" target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>
+          &nbsp;|&nbsp;
+          <a href="https://www.linkedin.com/in/rani-rangari/" target="_blank" rel="noopener noreferrer">
+            LinkedIn
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
